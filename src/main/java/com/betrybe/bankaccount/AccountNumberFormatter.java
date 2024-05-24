2bcfee5402802ea1f1ba420056af1c8d620716a3
Débora Serra <debora.r.serra@gmail.com>
2024-05-24 11:57:26 -0300

    feat: add requirement two

--- src/main/java/com/betrybe/bankaccount/AccountNumberFormatter.java
+++ src/main/java/com/betrybe/bankaccount/AccountNumberFormatter.java
@@ -12,7 +12,19 @@ public class AccountNumberFormatter {
    * @param literalAccountNumber número da conta
    */
   public String formatAccountNumber(int literalAccountNumber) {
-
+    String numberToString = Integer.toString((literalAccountNumber));
+    if (numberToString.length() == 6) return  numberToString;
+    if (numberToString.length() > 6) {
+      int start = numberToString.length() - 6;
+      String newAccount = numberToString.substring(start);
+      return  newAccount;
+    }
+    int leadingZeros = 6 - numberToString.length();
+    String start = "";
+    for (int i = 1; i <= leadingZeros; i +=1 ) {
+      start += "0";
+    }
+    return start + numberToString;
   }
 
 }
