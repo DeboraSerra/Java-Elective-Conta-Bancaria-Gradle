1a77ae86ddf19e585bf49169b2f159c0ed1e6c14
Débora Serra <debora.r.serra@gmail.com>
2024-05-24 12:15:17 -0300

    feat: add requirement three

--- src/main/java/com/betrybe/bankaccount/AccountNumberFormatter.java
+++ src/main/java/com/betrybe/bankaccount/AccountNumberFormatter.java
@@ -13,7 +13,9 @@ public class AccountNumberFormatter {
    */
   public String formatAccountNumber(int literalAccountNumber) {
     String numberToString = Integer.toString((literalAccountNumber));
-    if (numberToString.length() == 6) return  numberToString;
+    if (numberToString.length() == 6) {
+      return  numberToString;
+    }
     if (numberToString.length() > 6) {
       int start = numberToString.length() - 6;
       String newAccount = numberToString.substring(start);
@@ -21,7 +23,7 @@ public class AccountNumberFormatter {
     }
     int leadingZeros = 6 - numberToString.length();
     String start = "";
-    for (int i = 1; i <= leadingZeros; i +=1 ) {
+    for (int i = 1; i <= leadingZeros; i += 1)  {
       start += "0";
     }
     return start + numberToString;
--- src/main/java/com/betrybe/bankaccount/Application.java
+++ src/main/java/com/betrybe/bankaccount/Application.java
@@ -1,5 +1,8 @@
 package com.betrybe.bankaccount;
 
+import java.util.Scanner;
+import org.apache.commons.lang3.math.NumberUtils;
+
 /**
  * A class.
  */
@@ -9,7 +12,15 @@ public class Application {
    * A simple get method.
    */
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    String answer = scanner.next();
+    if (!NumberUtils.isCreatable(answer)) {
+      System.out.println("Número da conta inválido!");
+      return;
+    }
+    AccountNumberFormatter formatter = new AccountNumberFormatter();
+    String account = formatter.formatAccountNumber(Integer.parseInt(answer));
+    System.out.print("Número da conta: " + account);
   }
 
 }
